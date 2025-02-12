13d3f55afa8bf22f80516a8cb549bcc5839b60fb
Anders Pearson <anders@thraxil.org>
2025-02-12 21:23:34 +0000

    drop gomod2nix
    back to a simpler approach for now

--- shell.nix
+++ shell.nix
@@ -1,31 +1,15 @@
-{ pkgs ? (
-    let
-      inherit (builtins) fetchTree fromJSON readFile;
-      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
-    in
-    import (fetchTree nixpkgs.locked) {
-      overlays = [
-        (import "${fetchTree gomod2nix.locked}/overlay.nix")
-      ];
-    }
-  )
-, mkGoEnv ? pkgs.mkGoEnv
-, gomod2nix ? pkgs.gomod2nix
-}:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05-pre.tar.gz") {} }:
 
-let
-  goEnv = mkGoEnv { pwd = ./.; };
-in
 pkgs.mkShell {
-  packages = [
-    goEnv
-    gomod2nix
+  buildInputs = [
+    pkgs.go
+    pkgs.gcc
+    pkgs.libcap
+    pkgs.python310
   ];
-  FINCH_PORT="7777";
-  FINCH_DB_FILE="database.db";
-  FINCH_SECRET="secret_for_development";
-  FINCH_MEDIA_DIR="media";
-  FINCH_ITEMS_PER_PAGE="50";
-  FINCH_BASE_URL="http://localhost:7777";
-  FINCH_TEMPLATE_DIR="templates";
+
+  shellHook = ''
+  '';
+
+  MY_ENVIRONMENT_VARIABLE = "world";
 }
